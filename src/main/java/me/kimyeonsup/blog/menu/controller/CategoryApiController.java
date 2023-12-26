86f9891fa2b2ad8aa6bbae2b98b1fd4403da7600
dustjq1004 <dustjq1005@naver.com>
2023-12-27 06:02:45 +0900

    feat/blogMenu : controller 수정 삭제 api 추가

--- src/main/java/me/kimyeonsup/blog/menu/controller/CategoryApiController.java
+++ src/main/java/me/kimyeonsup/blog/menu/controller/CategoryApiController.java
@@ -3,11 +3,14 @@ package me.kimyeonsup.blog.menu.controller;
 import lombok.RequiredArgsConstructor;
 import me.kimyeonsup.blog.menu.domain.dto.AddCategoryRequest;
 import me.kimyeonsup.blog.menu.domain.dto.CategoryResponse;
-import me.kimyeonsup.blog.menu.domain.entity.Category;
+import me.kimyeonsup.blog.menu.domain.dto.UpdateCategoryRequest;
 import me.kimyeonsup.blog.menu.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +24,24 @@ public class CategoryApiController {
 
     @PostMapping("/category")
     public ResponseEntity<CategoryResponse> addCategory(@RequestBody AddCategoryRequest request) {
-        Category savedCategory = categoryService.save(request);
+        CategoryResponse savedCategory = categoryService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(new CategoryResponse(savedCategory));
+                .body(savedCategory);
+    }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable long id) {
+        categoryService.delete(id);
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PutMapping("/category/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable long id,
+                                                           @RequestBody UpdateCategoryRequest request) {
+        CategoryResponse updatedCategory = categoryService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedCategory);
     }
 }
