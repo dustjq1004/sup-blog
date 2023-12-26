673b6da2f6c024ed869f73a75e0dddb2a8d79613
dustjq1004 <dustjq1005@naver.com>
2023-12-27 06:03:26 +0900

    feat/blogMenu : service 도메인 클래스 대신 Dto 클래스 반환하도록 변경

--- src/main/java/me/kimyeonsup/blog/menu/service/CategoryService.java
+++ src/main/java/me/kimyeonsup/blog/menu/service/CategoryService.java
@@ -3,6 +3,7 @@ package me.kimyeonsup.blog.menu.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.kimyeonsup.blog.menu.domain.dto.AddCategoryRequest;
+import me.kimyeonsup.blog.menu.domain.dto.CategoryResponse;
 import me.kimyeonsup.blog.menu.domain.dto.UpdateCategoryRequest;
 import me.kimyeonsup.blog.menu.domain.entity.Category;
 import me.kimyeonsup.blog.menu.repository.CategoryRepository;
@@ -15,8 +16,8 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
-    public Category save(AddCategoryRequest request) {
-        return categoryRepository.save(request.toEntity());
+    public CategoryResponse save(AddCategoryRequest request) {
+        return new CategoryResponse(categoryRepository.save(request.toEntity()));
     }
 
     public List<Category> findAll() {
@@ -30,10 +31,10 @@ public class CategoryService {
     }
 
     @Transactional
-    public Category update(long id, UpdateCategoryRequest request) {
+    public CategoryResponse update(long id, UpdateCategoryRequest request) {
         Category category = categoryRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
         category.update(request.getName());
-        return category;
+        return new CategoryResponse(category);
     }
 }
