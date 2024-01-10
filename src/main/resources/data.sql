693c60e96f69368be6c416a93ab408704485f0a3
dustjq1004 <dustjq1005@naver.com>
2024-01-10 16:11:02 +0900

    data : menu data 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,11 @@
-insert into article (title, content, author, created_at, updated_at) values ('제목 1', '내용 1', 'user1', NOW(), NOW())
-insert into article (title, content, author, created_at, updated_at) values ('제목 2', '내용 2', 'user2', NOW(), NOW())
-insert into article (title, content, author, created_at, updated_at) values ('제목 3', '내용 3', 'user3', NOW(), NOW())
+insert into article (title, content, author, menu_id, created_at, updated_at) values ('제목 1', '내용 1', 'user1', 1, NOW(), NOW())
+insert into article (title, content, author, menu_id, created_at, updated_at) values ('제목 2', '내용 2', 'user2', 1, NOW(), NOW())
+insert into article (title, content, author, menu_id, created_at, updated_at) values ('제목 3', '내용 3', 'user3', 1, NOW(), NOW())
 
 insert into category (name) values ('언어')
+insert into category (name) values ('부트캠프')
 
 insert into menu (name, category_id) values ('자바', 1)
+insert into menu (name, category_id) values ('파이썬', 1)
+insert into menu (name, category_id) values ('javascript', 1)
+insert into menu (name, category_id) values ('우아한테크코스', 2)
