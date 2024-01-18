c79f59147fd30b83ae01f0465d8f32c376bed36e
dustjq1004 <dustjq1005@naver.com>
2024-01-18 19:28:35 +0900

    front : 메뉴 active 방식, 비동기 호출, 기타 스타일 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,3 @@
-insert into article (title, content, author, menu_id, created_at, updated_at) values ('제목 1', '내용 1', 'user1', 1, NOW(), NOW())
-insert into article (title, content, author, menu_id, created_at, updated_at) values ('제목 2', '내용 2', 'user2', 1, NOW(), NOW())
-insert into article (title, content, author, menu_id, created_at, updated_at) values ('제목 3', '내용 3', 'user3', 1, NOW(), NOW())
-
 insert into category (name) values ('언어')
 insert into category (name) values ('부트캠프')
 
@@ -9,3 +5,7 @@ insert into menu (name, category_id) values ('자바', 1)
 insert into menu (name, category_id) values ('파이썬', 1)
 insert into menu (name, category_id) values ('javascript', 1)
 insert into menu (name, category_id) values ('우아한테크코스', 2)
+
+insert into article (title, sub_title, content, author, menu_id, created_at, updated_at) values ('제목 1', 'subTitle 예시입니다.', '내용 1', 'user1', 1, NOW(), NOW())
+insert into article (title, sub_title, content, author, menu_id, created_at, updated_at) values ('제목 2', 'subTitle 예시입니다.', '내용 2', 'user2', 1, NOW(), NOW())
+insert into article (title, sub_title, content, author, menu_id, created_at, updated_at) values ('제목 3', 'subTitle 예시입니다.', '내용 3', 'user3', 1, NOW(), NOW())
--- src/main/resources/static/js/blog.js
+++ src/main/resources/static/js/blog.js
@@ -1,3 +1,21 @@
+// 메뉴 활성화
+document.addEventListener('DOMContentLoaded', function () {
+    // 현재 페이지 URL
+    let currentUrl = window.location.href;
+    let decodedUrl = decodeURIComponent(currentUrl);
+
+    // 모든 메뉴 항목 가져오기
+    let menuItems = document.querySelectorAll('.side-link a');
+
+    // 각 메뉴 항목에 대해 현재 페이지와 비교하여 active 클래스 추가
+    menuItems.forEach(function (menuItem) {
+        let menuName = menuItem.textContent;
+        if (decodedUrl.includes(menuName)) {
+            menuItem.classList.add('active');
+        }
+    });
+});
+
 const getArticles = (menuId) => {
     const data = {
         "menuId": menuId
@@ -12,20 +30,6 @@ const getArticles = (menuId) => {
 
     ajaxGetRequest('GET', '/blog/articles', data, success, fail);
 }
-
-const getArticle = (articleId) => {
-    function success(fragment) {
-        $('#main').html(fragment);
-    }
-
-    function fail() {
-    }
-
-    ajaxGetRequest('GET', '/blog/articles/' + articleId, null, success, fail);
-}
-
-
-getArticles();
 
 function ajaxGetRequest(method, url, data, success, fail) {
     $.ajax({
