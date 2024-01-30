0deb487af9594425933fbaa6f3a81b5c95b534bd
dustjq1004 <dustjq1005@naver.com>
2024-01-30 16:52:52 +0900

    front/refactor : Refactoring FrontEnd - common.js 생성 - 중복 코드 제거 - promise async await 구문으로 변경하여 가독성 증가

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,73 +1,9 @@
-const easyMDE = new EasyMDE({
-    status: ["lines", "words"],
-    spellChecker: false,
-    element: $("#content")[0],
-    promptURLs: true,
-    sideBySideFullscreen: false,
-    toolbarButtonClassPrefix: "mde",
-    imageUploadEndpoint: '/',
-    uploadImage: true,
-    toolbar: [
-        "bold", "italic", "strikethrough", "heading", "|",
-        "code", "quote", "unordered-list", "ordered-list", "table", "|",
-        "upload-image", "link", "|",
-        "preview", "side-by-side"
-    ],
-    theme: "Modern",
-    imageUploadFunction:
-        async (file, onSuccess, onError) => {
-            const result = await getResignedUrl(file.name);
-            const presigendUrl = result.signedUrl;
-            const s3Data = await requestS3PresignedImageUpload(presigendUrl, file.type, file);
-            onSuccess(s3Data.url.split("?")[0]);
-        }
-});
-
-const getResignedUrl = async (fileName) => {
-    const options = {
-        method: "GET",
-        headers: {
-            "Content-type": "application/json"
-        }
-    }
-    const response = await fetch("/api/s3/image?fileName=" + fileName, options);
-    return response.json();
-}
-
-const requestS3PresignedImageUpload = async (presigendUrl, type, file) => {
-    const response = await fetch(presigendUrl, {
-        method: "PUT",
-        body: file,
-        headers: {
-            "Content-type": type
-        }
-    });
-    return response;
-}
-
-// easyMDE.codemirror.on("paste", function () {
-//     var items = (event.clipboardData || event.originalEvent.clipboardData).items;
-//     console.log(JSON.stringify(items));
-//     for (index in items) {
-//         var item = items[index];
-//         if (item.kind === 'file') {
-//             var blob = item.getAsFile();
-//             var reader = new FileReader();
-//             reader.onload = function (event) {
-//                 // data url!
-//                 // console.log(event.target.result)
-//             };
-//             reader.readAsDataURL(blob);
-//         }
-//     }
-// });
-
 // 삭제 기능
 const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
-        let id = document.getElementById('article-id').value;
+        const id = document.getElementById('article-id').value;
 
         function success() {
             alert('삭제가 완료되었습니다.');
@@ -79,7 +15,11 @@ if (deleteButton) {
             location.replace('/blog');
         }
 
-        httpRequest('DELETE', `/api/articles/${id}`, null, success, fail);
+        const options = {
+            method: 'DELETE'
+        };
+
+        httpRequest(`/api/articles/${id}`, options, success, fail);
     });
 }
 
@@ -90,26 +30,32 @@ if (modifyButton) {
     modifyButton.addEventListener('click', event => {
         let params = new URLSearchParams(location.search);
         let id = params.get('id');
-        const content = easyMDE.value()
-        ;
-        console.log(content);
+        const content = easyMDE.value();
+        const selectElement = document.getElementById('menu');
+        const selectedMenu = selectElement.options[selectElement.selectedIndex];
+        const menuName = selectedMenu.text;
         body = JSON.stringify({
             title: document.getElementById('title').value,
             content: content,
-            menuId: document.getElementById('menu').value
+            menuId: selectElement.value
         });
 
         function success() {
             alert('수정 완료되었습니다.');
-            location.replace(`/articles/${id}`);
+            location.replace(`/blog/${menuName}/${id}`);
         }
 
         function fail() {
             alert('수정 실패했습니다.');
-            location.replace(`/articles/${id}`);
+            location.replace(`/blog/${menuName}/${id}`);
         }
 
-        httpRequest('PUT', `/api/articles/${id}`, body, success, fail);
+        const options = {
+            method: 'PUT',
+            body: body
+        };
+
+        httpRequest(`/api/articles/${id}`, options, success, fail);
     });
 }
 
@@ -121,7 +67,7 @@ if (createButton) {
     createButton.addEventListener('click', event => {
         const content = easyMDE.value();
         console.log(content);
-        body = JSON.stringify({
+        const body = JSON.stringify({
             title: document.getElementById('title').value,
             content: content,
             menuId: document.getElementById('menu').value
@@ -137,66 +83,12 @@ if (createButton) {
             location.replace('/blog');
         };
 
-        httpRequest('POST', '/api/articles', body, success, fail)
+        const options = {
+            method: 'POST',
+            body: body
+        };
+
+        httpRequest('/api/articles', options, success, fail)
     });
 }
 
-
-// 쿠키를 가져오는 함수
-function getCookie(key) {
-    var result = null;
-    var cookie = document.cookie.split(';');
-    cookie.some(function (item) {
-        item = item.replace(' ', '');
-
-        var dic = item.split('=');
-
-        if (key === dic[0]) {
-            result = dic[1];
-            return true;
-        }
-    });
-
-    return result;
-}
-
-// HTTP 요청을 보내는 함수
-function httpRequest(method, url, body, success, fail) {
-    fetch(url, {
-        method: method,
-        headers: { // 로컬 스토리지에서 액세스 토큰 값을 가져와 헤더에 추가
-            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
-            'Content-Type': 'application/json',
-        },
-        body: body,
-    }).then(response => {
-        if (response.status === 200 || response.status === 201) {
-            return success();
-        }
-        const refresh_token = getCookie('refresh_token');
-        if (response.status === 401 && refresh_token) {
-            fetch('/api/token', {
-                method: 'POST',
-                headers: {
-                    Authorization: 'Bearer ' + localStorage.getItem('access_token'),
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    refreshToken: getCookie('refresh_token'),
-                }),
-            })
-                .then(res => {
-                    if (res.ok) {
-                        return res.json();
-                    }
-                })
-                .then(result => { // 재발급이 성공하면 로컬 스토리지값을 새로운 액세스 토큰으로 교체
-                    localStorage.setItem('access_token', result.accessToken);
-                    httpRequest(method, url, body, success, fail);
-                })
-                .catch(error => fail());
-        } else {
-            return fail();
-        }
-    });
-}
