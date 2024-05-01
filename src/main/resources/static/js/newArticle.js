54a9d3e553262651eb83889700a614f452481bae
dustjq1004 <dustjq1005@naver.com>
2024-05-01 16:09:29 +0900

    front : 임시저장 기능 추가

---
+++ src/main/resources/static/js/newArticle.js
@@ -0,0 +1,57 @@
+let draftList = {};
+
+const success = (json) => {
+    draftList = json;
+    html = setDraftList(json);
+
+    $('#draft-list').html(html);
+}
+
+function setDraftList(json) {
+    let html = "";
+    let index = 0;
+
+    if (json.length <= 0) return html = `<li><span class="dropdown-item-text">임시로 저장된 게시글이 없습니다.</span></li>`
+
+    for (let draft of json) {
+        html += `<li><a class="dropdown-item" onclick="loadDraft(${index++})">${draft.title}</a></li>`
+    }
+    return html;
+}
+
+function loadDraft(index) {
+    const draft = draftList[index];
+    if (!confirm(`임시저장한 게시글 ${draft.title}을 불러오시겠습니까?`)) return
+
+    resetForm();
+
+    $('#draft-id').val(draft.id);
+    $('#menu').val(draft.menuId);
+    $('#title').val(draft.title);
+    $('#subTitle').val(draft.subTitle);
+    easyMDE.value(draft.content);
+}
+
+function resetForm() {
+    $('form').each(function () {
+        this.reset();
+    });
+
+    easyMDE.value("");
+}
+
+$(window).bind('beforeunload', function () {
+    if (isDraftSaved)
+        return '페이지를 나가시겠습니까? 기존에 작성한 글은 복구할 수 없습니다.';
+});
+
+$(document).ready(function () {
+    $('#draft-button').click((event) => {
+        const options = {
+            method: 'GET'
+        };
+
+        httpRequest('/api/draft', options, success, () => {
+        })
+    });
+});
