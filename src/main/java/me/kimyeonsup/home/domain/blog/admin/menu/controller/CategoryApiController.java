d3cbf2b61cbfe5293b0c00332c4cafcebae87b76
dustjq1004 <dustjq1005@naver.com>
2024-12-07 23:28:08 +0900

    feat : Categories, menus API GET 방식 추가

--- src/main/java/me/kimyeonsup/home/domain/blog/admin/menu/controller/CategoryApiController.java
+++ src/main/java/me/kimyeonsup/home/domain/blog/admin/menu/controller/CategoryApiController.java
@@ -1,14 +1,23 @@
 package me.kimyeonsup.home.domain.blog.admin.menu.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.kimyeonsup.home.domain.blog.admin.menu.domain.dto.AddCategoryRequest;
 import me.kimyeonsup.home.domain.blog.admin.menu.domain.dto.CategoryResponse;
 import me.kimyeonsup.home.domain.blog.admin.menu.domain.dto.UpdateCategoryRequest;
+import me.kimyeonsup.home.domain.blog.admin.menu.domain.entity.Category;
 import me.kimyeonsup.home.domain.blog.admin.menu.service.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
@@ -16,6 +25,12 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryApiController {
 
     private final CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> findCategories() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(categoryService.findAll());
+    }
 
     @PostMapping("/category")
     public ResponseEntity<CategoryResponse> addCategory(@Validated @RequestBody AddCategoryRequest request) {
--- src/main/java/me/kimyeonsup/home/domain/blog/admin/menu/controller/MenuApiController.java
+++ src/main/java/me/kimyeonsup/home/domain/blog/admin/menu/controller/MenuApiController.java
@@ -1,14 +1,23 @@
 package me.kimyeonsup.home.domain.blog.admin.menu.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import me.kimyeonsup.home.domain.blog.admin.menu.domain.dto.MenuResponse;
 import me.kimyeonsup.home.domain.blog.admin.menu.domain.dto.AddMenuRequest;
+import me.kimyeonsup.home.domain.blog.admin.menu.domain.dto.MenuResponse;
 import me.kimyeonsup.home.domain.blog.admin.menu.domain.dto.UpdateMenuRequest;
+import me.kimyeonsup.home.domain.blog.admin.menu.domain.entity.Menu;
 import me.kimyeonsup.home.domain.blog.admin.menu.service.MenuService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
@@ -16,6 +25,12 @@ import org.springframework.web.bind.annotation.*;
 public class MenuApiController {
 
     private final MenuService menuService;
+
+    @GetMapping("/menus")
+    public ResponseEntity<List<Menu>> findCategories() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(menuService.findAll());
+    }
 
     @PostMapping("/menu")
     public ResponseEntity<MenuResponse> addMenu(@Validated @RequestBody AddMenuRequest request) {
