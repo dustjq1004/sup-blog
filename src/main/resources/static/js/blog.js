28405d6a129c4b8ed2cc90b91abfbdb787801117
dustjq1004 <dustjq1005@naver.com>
2025-02-09 23:32:13 +0900

    fix : #52

--- src/main/resources/static/js/blog.js
+++ src/main/resources/static/js/blog.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let decodedUrl = decodeURIComponent(currentUrl);
 
     // 모든 메뉴 항목 가져오기
-    let menuName = decodedUrl.split("/").reverse()[0];
-    
+    let menuName = decodedUrl.split("/")[5];
+
     // 각 메뉴 항목에 대해 현재 페이지와 비교하여 active 클래스 추가
     menuItems.forEach(function (menuItem) {
         let menuName = menuItem.textContent;
