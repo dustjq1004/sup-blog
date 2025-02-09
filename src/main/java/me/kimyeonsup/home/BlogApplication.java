7f0b98d5ea8119e8a7495a25fea66c7717f97605
dustjq1004 <dustjq1005@naver.com>
2025-02-09 20:03:25 +0900

    feat : #53 - commonMark Table extension 추가 - local profile Env 설정 추가

--- src/main/java/me/kimyeonsup/home/BlogApplication.java
+++ src/main/java/me/kimyeonsup/home/BlogApplication.java
@@ -1,5 +1,7 @@
 package me.kimyeonsup.home;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
+
+    @PostConstruct
+    public void loadEnv() {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
