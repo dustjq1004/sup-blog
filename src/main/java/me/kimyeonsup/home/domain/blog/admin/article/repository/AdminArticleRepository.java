515625555bf5c0af2d4e28a7d1e30ff679c695a9
dustjq1004 <dustjq1005@naver.com>
2025-04-30 22:03:59 +0900

    feat: AdminArticleRepository 메서드 수정

--- src/main/java/me/kimyeonsup/home/domain/blog/admin/article/repository/AdminArticleRepository.java
+++ src/main/java/me/kimyeonsup/home/domain/blog/admin/article/repository/AdminArticleRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AdminArticleRepository extends JpaRepository<AdminArticle, Long> {
 
-    List<AdminArticle> findByCategoryId(Long categoryId);
+    List<AdminArticle> findByMenu_CategoryId(Long categoryId);
 
     List<AdminArticle> findByMenuId(Long menuId);
 }
